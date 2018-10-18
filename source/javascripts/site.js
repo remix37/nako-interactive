80792472b035ac618d6394c8101b967d72c398f4
Rigault Rémy <r.rigault@me.com>
2018-10-18 12:00:46 +0200

    add navbar-fix and footer

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,3 +3,19 @@
 //= require jquery-3.3.1.slim.min/index.js
 //= require popper.min/index.js
 //= require boostrap/dist/js/bootstrap.js
+
+$(document).ready(function(){
+    var scroll_start = 0;
+    var startchange = $('.top-section');
+    var offset = startchange.offset();
+    if (startchange.length){
+        $(document).scroll(function() {
+            scroll_start = $(document).scrollTop();
+            if(scroll_start > offset.top) {
+                $(".navbar").addClass("navbar-light bg-light").removeClass("navbar-dark");
+            } else {
+                $(".navbar").addClass("navbar-dark").removeClass("navbar-light bg-light");
+            }
+        });
+    }
+ });
